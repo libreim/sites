b8fed83d912da0f92b1e4d7da76b4a7cca407d35
David Charte <fdavidcl@outlook.com>
2016-03-05 16:43:51 +0100

    More updates to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
 build() {
+  local defaults=../_defaults.yml
   local config=_config.yml
   local tux=_tux.yml
 
   if [ -f $config ]; then
+    config="$defaults,$config"
+
     if [ -f $tux ]; then
       config="$config,$tux"
     fi
@@ -19,13 +22,13 @@ deploy_all() {
   local site_path=.site
 
   for site in `find *  -maxdepth 0 ! -path '*/.*' ! -path '*/_*' -type d`; do
-    rm $site_path/$site -r
+    rm $site_path/$site -rf
     echo "Entrando en $site"
     cd $site
     build
     mkdir -p ../$site_path/$site
     mv _site/* ../$site_path/$site
-    rm -r _site/*
+    rm -rf _site/*
     echo "Saliendo de $site"
     cd ..
   done
