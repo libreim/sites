4cfdb79b1d668bf337ab716153db0ea2e636015c
David Charte <fdavidcl@outlook.com>
2016-03-05 12:53:36 +0100

    First attempt at travis deploying everything

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+build() {
+  local config=_config.yml
+  local tux=_tux.yml
+
+  if [ -f $tux ]
+    config=$config,$tux
+  fi
+
+  bundle exec jekyll build --config $config
+}
+
+deploy_all() {
+  local site_path=_site
+
+  for site in `find , -type d`
+  do
+    cd $site
+    build
+    mkdir -p ../$site_path/$site
+    mv _site/* ../$site_path/$site
+  done
+}
+
+deploy_all
