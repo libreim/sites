cd21e36969ef65c962b6a6d7736459c9b1027100
David Charte <fdavidcl@outlook.com>
2016-03-05 13:55:15 +0100

    Improve script

--- deploy.sh
+++ deploy.sh
@@ -2,23 +2,31 @@ build() {
   local config=_config.yml
   local tux=_tux.yml
 
-  if [ -f $tux ]
-    config=$config,$tux
-  fi
+  if [ -f $config ]; then
+    if [ -f $tux ]; then
+      config="$config,$tux"
+    fi
 
-  bundle install
-  bundle exec jekyll build --config $config
+    bundle install
+    bundle exec jekyll build --config $config
+  else
+    mkdir -p _site
+    cp * _site -r
+  fi
 }
 
 deploy_all() {
-  local site_path=_site
+  local site_path=.site
 
-  for site in `find , -type d`
-  do
+  for site in `find *  -maxdepth 0 ! -path '*/.*' ! -path '*/_*' -type d`; do
+    echo "Entrando en $site"
     cd $site
     build
     mkdir -p ../$site_path/$site
     mv _site/* ../$site_path/$site
+    rm -r _site/*
+    echo "Saliendo de $site"
+    cd ..
   done
 }
 
